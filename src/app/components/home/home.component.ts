6dc7ec2487df6adf82f3fc9af2d9c1d5036eec58
Kaumini Gunasinghe <kauminikg@gmail.com>
2019-03-21 06:02:12 +0800

    markers and polyline added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { Point } from '../../models/Point';
+import { Marker } from '../../models/marker';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,44 @@ title = 'app';
 lat: number = 7.8731;
 lng: number = 80.7718;
 
-  constructor() { }
+
+  constructor() { 
+    // var map;
+    // var p:Point = {
+    //   lat: 1.5,
+    //   lng: 2.5
+    // }
+    // var points: any[] = [];
+    // points.push(p);
+  }
 
   ngOnInit() {
   }
+
+  placeMarker($event){
+    console.log($event.coords.lat);
+    console.log($event.coords.lng);
+    this.markers.push(
+      {
+        lat: $event.coords.lat,
+        lng: $event.coords.lng
+      }
+    );
+    this.paths.push(
+      {
+        lat: $event.coords.lat,
+        lng: $event.coords.lng
+      }
+    );
+    console.log(this.markers);
+    console.log(this.paths);
+  }
+  
+  markers: Marker[] = [
+  ]
+  paths: Array<Marker> = [
+         { lat: 10, lng: 10 },
+         { lat: 0,  lng: 10 }
+       ]
 
 }
